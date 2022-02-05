2dae7364ab00e9d8c93cbae5b192bd36c8379039
Shambashib Majumdar <62923672+shambashib20@users.noreply.github.com>
2022-02-05 18:09:55 +0530

    Time: 15 ms (24.41%), Space: 47.3 MB (8.81%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        
+        ListNode head = new ListNode(0); // dummy node head
+        ListNode temp = head; // taking into temporary variable
+        List<Integer> l = new ArrayList<>();
+        for(ListNode list : lists){ // adding all the values in the list
+            while(list != null){
+                l.add(list.val);
+                list = list.next;
+            }
+        }
+        Collections.sort(l); // sorting that list we get above
+        for(int val : l){ // iterating over the list & creating new single linked list
+            temp.next = new ListNode(val);
+            temp = temp.next;
+        }
+        return head.next;
+    }
+}
