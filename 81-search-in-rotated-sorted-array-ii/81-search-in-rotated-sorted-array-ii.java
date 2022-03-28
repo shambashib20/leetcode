3136316b1707e986f1776f1d57ea25b9314efb9c
Shambashib Majumdar <62923672+shambashib20@users.noreply.github.com>
2022-03-28 19:25:58 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (87.01%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        for(int i : nums){
+            if(i == target) return true;
+        }
+        return false;
+    }
+}
