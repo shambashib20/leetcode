a88f67c0c615262d63d50da43acacfb29fe81732
Shambashib Majumdar <62923672+shambashib20@users.noreply.github.com>
2022-06-28 15:23:51 +0530

    Time: 80 ms (27.16%), Space: 45 MB (58.48%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int max = nums.length;
+        
+        for(int i = 0;i<max;i++)
+        {
+            int diff = target - nums[i];
+            for(int j=i+1;j<max;j++)
+            {
+                if(nums[j] == diff)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{0,0};
+    }
+}
